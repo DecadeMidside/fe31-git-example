583f3d531b979e89a7213471e641eae445b5fcbd
DecadeMidside <hoanglongskbi@gmail.com>
2023-03-01 19:54:50 +0700

    fe-31-git-test

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer, { name, className } from "./Footer";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Main />
+      <Footer />
+      {name} - {className}
+    </div>
+  );
+}
+
+export default App;
